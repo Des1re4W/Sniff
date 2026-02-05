76bad308a7a8efde5aaddea5a1b51f12e36c7756
SHAN BENZER A. FAJAGUTANA <zika.buyag@gmail.com>
2026-02-05 18:51:26 +0800

    pls

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -21,7 +21,7 @@ function LocationSection() {
     </div>
 
         <a
-          href="https://www.google.com/maps/dir/6.3635456,124.7412224/SNIFF+PREMIUM+DETAILING+WASH,+INFRONT+OF+SURALLAH+ICE+PLANT+(TULOY+PO,+BARANGAY+ZONE+2B,+Surallah,+9506/@6.3735267,124.7366075,15z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x32f7f72286209f4d:0x1bae8dbd9563843d!2m2!1d124.7531904!2d6.3833181?entry=ttu&g_ep=EgoyMDI2MDEyNi4wIKXMDSoASAFQAw%3D%3D"
+          href="https://www.google.com/maps/dir//SNIFF+PREMIUM+DETAILING+WASH,+INFRONT+OF+SURALLAH+ICE+PLANT+(TULOY+PO,+BARANGAY+ZONE+2B,+Surallah,+9506/@6.3735267,124.7366074,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0x32f7f72286209f4d:0x1bae8dbd9563843d!2m2!1d124.7531904!2d6.3833181?entry=ttu&g_ep=EgoyMDI2MDIwMy4wIKXMDSoASAFQAw%3D%3D"
           target="_blank"
           rel="noopener noreferrer"
           className="route-btn"
